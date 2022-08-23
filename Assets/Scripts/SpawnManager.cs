ad96a2fb739f602c9afafb90b0b8636e8bab8b20
GmoZ87 <109632262+GmoZ87@users.noreply.github.com>
2022-08-22 23:05:15 -0300

    Cambios
    Principios de escenario y spawns

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+
+{
+    [SerializeField] GameObject Spawned;
+    [SerializeField][Range(1f, 20f)] float time = 1f;
+    [SerializeField][Range(1f, 30f)] float repeatRate = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawns", time, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Spawns()
+    {
+        Instantiate(Spawned, transform.position, transform.rotation);
+    }
+}
+
