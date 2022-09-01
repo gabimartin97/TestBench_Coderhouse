b1229627c994caf7ae9bbedee5f9e298f1f67ebf
Gabriel Martin <gabriel97martin@gmail.com>
2022-09-01 19:58:44 -0300

    Ahora los enemigos se mueven con NavMesh
    Siguiendo las indicaciones del after de point and click

---
+++ Assets/Scripts/EnemyAgent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAgent : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        if(direction.magnitude >= 2f)
+        {
+            navMeshAgent.SetDestination(target.transform.position);
+        }
+        
+    }
+}
